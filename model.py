37e40d7bcc67fdeaf2aed37c4d65064e2ce13918
Sarah <sarahhtbarton@gmail.com>
2021-04-06 21:01:20 +0000

    adds feedback comments from model.py code review with Thu

--- model.py
+++ model.py
@@ -18,7 +18,8 @@ class LetterInput(db.Model):
     required_letter = db.Column(db.String(1))
     additional_letters = db.Column(db.String(6))
 
-    # do I need a relationship???
+    # do I need a relationship??? do for the one-to-manys. Create relationship anywhere there's a foreign key. Do a backref -- easier to join
+    # if a foreign key in a table, do a backref to the table where the foreign key is the primary key
 
     def __repr__(self):
         return f'<LetterInput
@@ -31,7 +32,7 @@ class LetterInput(db.Model):
 class LetterWordAssoc(db.Model):
     """An association table that holds the words for the day's Spelling Bee letters"""
 
-    __tablename__ = 'LetterWordAssoc' #does CamelCase work here?
+    __tablename__ = 'letterword_assoc' #does CamelCase work here? No.
 
     letterword_id = db.Column(db.Integer,
                               autoincrement=True,
@@ -44,7 +45,7 @@ class LetterWordAssoc(db.Model):
                                    db.ForeignKey('word_masterlist.word_masterlist_id')
                                    )
     
-    # do I need a relationship???
+    # do I need a relationship??? Secondary???? Secondary is not necessary. Good to use in many-to-many.
 
     def __repr__(self):
         return f'<LetterWordAssoc
@@ -62,7 +63,7 @@ class WordMasterlist(db.Model):
     word_masterlist_id = db.Column(db.Integer,
                                   autoincrement=True,
                                   primary_key=True
-                                  )
+                                  ) #make unique
     word = db.Column(db.String)
 
      # do I need a relationship???
@@ -75,7 +76,7 @@ class WordMasterlist(db.Model):
 
 
 class WordFeedback(db.Model):
-    """User input on words Solver said were valid but Spelling Bee said were not
+    """User input on words Solver said were valid but Spelling Bee said were not,
        or visa versa"""
     
     __tablename__ = 'word_feedback'
@@ -88,9 +89,9 @@ class WordFeedback(db.Model):
                                    db.ForeignKey('word_masterlist.word_masterlist_id')
                                    )
     is_blacklisted = db.Column(db.Boolean) #is this how you do boolean?
-    is_whitelisted = db.Column(db.Boolean))
-
-# do I need a relationship???
+    is_whitelisted = db.Column(db.Boolean) #could set a default value *****
+    
+    # do I need a relationship???
 
     def __repr__(self):
         return f'<WordFeedback
@@ -102,7 +103,9 @@ class WordFeedback(db.Model):
 
 #i copy-pasted everything below from the ratings lab. What should I keep (and what does it do?)
 
-def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
+def connect_to_db(flask_app,
+                  db_uri='postgresql:///ratings', #need to name the DB!! the part that come after postgresql://// 
+                  echo=True): #echo can be a bit annoying. Can turn to false
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     flask_app.config['SQLALCHEMY_ECHO'] = echo
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -116,8 +119,6 @@ def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
 if __name__ == '__main__':
     from server import app
 
-    # Call connect_to_db(app, echo=False) if your program output gets
-    # too annoying; this will tell SQLAlchemy not to print out every
-    # query it executes.
+#will add something here if deploy
 
     connect_to_db(app)
