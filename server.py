1f53c10ce966b37f7da2658f13dd7ececcac56bd
Sarah <sarahhtbarton@gmail.com>
2021-04-07 18:42:23 +0000

    creates barebones server.py

---
+++ server.py
@@ -0,0 +1,12 @@
+"""Server for Spelling Bee Solver app."""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# Replace this with routes and view functions!
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
