a18cc582b7435239942198416d4916a2ba68b3b9
Sarah <sarahhtbarton@gmail.com>
2021-04-19 19:03:48 +0000

    Uses Regex in SQL Alchemy filter to address runtime issues

--- crud.py
+++ crud.py
@@ -44,19 +44,11 @@ def create_assoc_table(letter_input_id, word_masterlist_id):
 def create_assoc_logic(letters_record):
     """Creates an association table for today's valid words"""
     
-    word_masterlist_objects = WordMasterlist.query.filter(WordMasterlist.word.like(f"%{letters_record.required_letter}%")).all()
-    #Alternative approaches:
-        # make a set of strings -> ask is this sequence in the set. Do this at the very beginning -- make global variable.
-        # define and call function is server.py
-        # update when make changes to master wordlist
-        #if YES, database query to get the primary key that you need later in the function (line 64)
-
-    pattern = f"[{letters_record.all_letters}]*{letters_record.required_letter}+[{letters_record.all_letters}]*"
-    compiled = re.compile(pattern)
+    pattern = f"^[{letters_record.all_letters}]*{letters_record.required_letter}+[{letters_record.all_letters}]*$"
+    word_masterlist_objects = WordMasterlist.query.filter(WordMasterlist.word.op('~')(f"{pattern}")).all()
 
     for object in word_masterlist_objects:
-        if compiled.fullmatch(object.word) is not None:
-            create_assoc_table(letters_record.letter_input_id, object.word_masterlist_id)
+        create_assoc_table(letters_record.letter_input_id, object.word_masterlist_id)
 
 
 if __name__ == '__main__':
