540ccda46bb537ac69bdacac17578f64eeb7ab4e
Sarah <sarahhtbarton@gmail.com>
2021-05-02 15:22:55 +0000

    code freeze

--- tests.py
+++ tests.py
@@ -7,8 +7,8 @@ class SolverIntegrationTests(unittest.TestCase):
     """Integration Tests -- Testing Flask app/server -- checks routes."""
 
     def setUp(self):
-        self.client = server.app.test_client() # Get the Flask test client
-        server.app.config['TESTING'] = True # Show Flask errors that happen during tests
+        self.client = server.app.test_client()
+        server.app.config['TESTING'] = True
         connect_to_db(server.app, "postgresql:///solver")
     
     def test_homepage(self):
@@ -23,8 +23,8 @@ class SolverIntegrationTests(unittest.TestCase):
     
     def test_post_form(self):
         result = self.client.post('/ajax-create-letters', data={'required-letter': 'l', 'all-letters': 'ncweaol'})
-        self.assertIn(b'allowance', result.data) #`data` is a dictionary of form key/value pairs. `result.data` is the html response string
+        self.assertIn(b'allowance', result.data)
 
 
-if __name__ == "__main__": # If called like a script, run our tests
+if __name__ == "__main__":
     unittest.main()
