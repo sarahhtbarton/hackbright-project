ec7fae5034252ea98197b8a2d9004b083e64ace1
Sarah <sarahhtbarton@gmail.com>
2021-05-02 15:07:37 +0000

    adds different div classes for whitelisted and blacklisted words

--- static/ajax_create_letters.js
+++ static/ajax_create_letters.js
@@ -1,21 +1,24 @@
+// "use strict";
 
 $('#get-letters').on('submit', (evt) => {
     evt.preventDefault();
-    // alert("We've received your submission of today's letters. It takes about 2 minutes to for the solutions to show up, so take a little stretch break and check back soon.");
 
     const formData = {
         "all-letters": $('[name="all-letters"]').val(),
         "required-letter": $('[name="required-letter"]').val()
     };
 
-    // make a post request -- POSTING something to database. Get = Read, Post = write
     $.post('/ajax-create-letters', formData, (res) => {
-        console.log(res.words);
+        console.log(res);
 
-        let wordsList = res.words;
-        // add a header? Like <h1>TODAYS ANSWERS</h1?
-        for (const word of wordsList) {
-            $('#append-words-here').append(`<div>${word}</div>`);
+        for (word in res) {
+            if (res[word]['blacklist_count'] > 0) {
+                $('#append-words-here').append(`<div class="blacklisted">${word}</div>`);
+            } else if (res[word]['whitelistlist_count'] > 0) {
+                $('#append-words-here').append(`<div class="whitelisted">${word}</div>`);
+            } else {
+                $('#append-words-here').append(`<div>${word}</div>`);
+            }
         }
     });
 });
